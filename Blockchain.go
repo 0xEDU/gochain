4c5f09dae1b9c916c62ef083298025e0b6312b64
edu <bfwaend@gmail.com>
2024-01-20 01:46:06 -0300

    Add Blockchain related functions

---
+++ Blockchain.go
@@ -0,0 +1,13 @@
+package main
+
+// Add a new block to Blockchain
+func (blockchain *Blockchain) AddBlock(data string) {
+	PreviousBlock := blockchain.Blocks[len(blockchain.Blocks)-1]
+	newBlock := NewBlock(data, PreviousBlock.PreviousBlockHash)
+	blockchain.Blocks = append(blockchain.Blocks, newBlock)
+}
+
+// Create a new blockchain
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
