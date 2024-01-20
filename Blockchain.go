47cb787d531260e83b9060108be6e3926a156f16
edu <bfwaend@gmail.com>
2024-01-20 01:52:38 -0300

    Fix wrong block hashing

--- Blockchain.go
+++ Blockchain.go
@@ -3,7 +3,7 @@ package main
 // Add a new block to Blockchain
 func (blockchain *Blockchain) AddBlock(data string) {
 	PreviousBlock := blockchain.Blocks[len(blockchain.Blocks)-1]
-	newBlock := NewBlock(data, PreviousBlock.PreviousBlockHash)
+	newBlock := NewBlock(data, PreviousBlock.MyBlockHash)
 	blockchain.Blocks = append(blockchain.Blocks, newBlock)
 }
 
