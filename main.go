e130921f8916155248a3bd9c1400d407023a6d8d
edu <bfwaend@gmail.com>
2024-01-20 01:55:57 -0300

    Print Block ID and timestamp on main

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ func main() {
 
 	newBlockchain.AddBlock("First transaction")
 	newBlockchain.AddBlock("Second transaction")
-	for _, block := range newBlockchain.Blocks {
+	for i, block := range newBlockchain.Blocks {
+		fmt.Printf("Block ID: %d\n", i)
+		fmt.Printf("Timestamp: %d\n", block.Timestamp+int64(i))
 		fmt.Printf("Hash of the Block: %x\n", block.MyBlockHash)
 		fmt.Printf("Hash of the previous Block: %x\n", block.PreviousBlockHash)
 		fmt.Printf("All the transactions: %s\n", block.AllData)
